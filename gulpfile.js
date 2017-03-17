09dfb6a775f332cc5d95bb122de00a55563f5f80
Mike Peiman <mikepeiman@gmail.com>
2017-03-17 10:11:36 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+/* 
+gulp.task('task-name', function() {
+	stuff here
+});
+*/
+gulp.task('hello', function() {
+	console.log('Hello, Mike')
+});
+/*
+gulp.task('task-name', function() {
+	return gulp.src('source-files')
+		.pipe(aGulpPlugin())
+		.pipe(gulp.dest('destination'))
+});
+*/
